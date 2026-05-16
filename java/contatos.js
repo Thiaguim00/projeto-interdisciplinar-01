703254cc5082ddaacbf0b728d9c8e31bbba15e63
Thiaguim00 <thiagohenque10050@gmail.com>
2026-05-16 17:06:44 -0300

    att

--- java/contatos.js
+++ java/contatos.js
@@ -1,35 +1,107 @@
+console.log("JS carregou");
+
+const api = "http://localhost:3000/contatos";
+
 function abrirModal() {
+
     document.getElementById("overlay").style.display = "flex";
+
 }
 
 function fecharModal() {
+
     document.getElementById("overlay").style.display = "none";
+
 }
 
-function adicionarContato() {
+async function adicionarContato() {
 
     let nome = document.getElementById("nome").value;
+
     let telefone = document.getElementById("telefone").value;
+
     let email = document.getElementById("email").value;
 
     if (nome === "" || telefone === "" || email === "") {
+
         alert("Preencha todos os campos!");
+
         return;
     }
 
-    let lista = document.getElementById("listaContatos");
+    const contato = {
+        nome: nome,
+        telefone: telefone,
+        email: email
+    };
 
-    lista.innerHTML += `
-        <div class="contato">
-            <h3>${nome}</h3>
-            <p>📞 ${telefone}</p>
-            <p>✉️ ${email}</p>
-        </div>
-    `;
+    await fetch(api, {
+
+        method: "POST",
+
+        headers: {
+            "Content-Type": "application/json"
+        },
+
+        body: JSON.stringify(contato)
+
+    });
 
     document.getElementById("nome").value = "";
+
     document.getElementById("telefone").value = "";
+
     document.getElementById("email").value = "";
 
     fecharModal();
+
+    carregarContatos();
 }
+
+async function carregarContatos() {
+
+    const resposta = await fetch(api);
+
+    const contatos = await resposta.json();
+
+    let lista = document.getElementById("listaContatos");
+
+    lista.innerHTML = "";
+
+    contatos.forEach(contato => {
+
+        lista.innerHTML += `
+
+            <div class="contato">
+
+                <h3>${contato.nome}</h3>
+
+                <p>📞 ${contato.telefone}</p>
+
+                <p>✉️ ${contato.email}</p>
+
+                <button class="btn-delete"
+                    onclick="deletarContato('${contato.id}')">
+
+                    Deletar
+
+                </button>
+
+            </div>
+
+        `;
+    });
+}
+
+async function deletarContato(id) {
+
+    await fetch(`http://localhost:3000/contatos/${id}`, {
+
+        method: "DELETE"
+
+    });
+
+    carregarContatos();
+}
+
+carregarContatos();
